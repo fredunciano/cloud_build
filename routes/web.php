85fe2d7f44f7584fd91b4a67d3625ea6f0cf42bf
fredunciano <webcareer1@gmail.com>
2019-08-13 10:41:56 +0800

    aa

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/log/{message}', function ($message) {
+    Log::info("Hello my log, message: $message");
+    return view('welcome');
+});
+
+Route::get('/exception/{message}', function ($message) {
+    throw new Exception("Intentional exception, message: $message");
+});
